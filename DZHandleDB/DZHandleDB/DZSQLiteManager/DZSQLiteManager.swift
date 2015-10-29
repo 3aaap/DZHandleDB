6ace2ccbc8a99774342c9e28a354e88b285d4add
song_dzhong <sdz96966@126.com>
2015-10-30 00:07:22 +0800

    01-项目初始化
    添加相应的类，并定义单例对象

---
+++ DZHandleDB/DZHandleDB/DZSQLiteManager/DZSQLiteManager.swift
@@ -0,0 +1,17 @@
+//
+//  DZSQLiteManager.swift
+//  DZHandleDB
+//
+//  Created by 宋得中 on 15/10/29.
+//  Copyright © 2015年 song_dzhong. All rights reserved.
+//
+
+import Foundation
+
+//MARK: SQLite 操作核心类
+class DZSQLiteManager {
+    /// 创建单例对象
+    static let sharedManager = DZSQLiteManager()
+    
+    
+}
---
+++ DZHandleDB/DZHandleDB/DZSQLiteManager/SQLite-Bridge.h
@@ -0,0 +1,9 @@
+//
+//  SQLite-Bridge.h
+//  DZHandleDB
+//
+//  Created by 宋得中 on 15/10/29.
+//  Copyright © 2015年 song_dzhong. All rights reserved.
+//
+
+#import <sqlite3.h>
