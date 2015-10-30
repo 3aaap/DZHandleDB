c898db53b555be896d753751c06e664bf6a8e007
song_dzhong <sdz96966@126.com>
2015-10-30 13:36:23 +0800

    04-测试封装的数据修改操作
    测试封装的数据修改操作，修改不妥当的地方

--- DZHandleDB/DZHandleDB/Person.swift
+++ DZHandleDB/DZHandleDB/Person.swift
@@ -26,3 +26,39 @@ class Person: NSObject {
         return dictionaryWithValuesForKeys(keys).description
     }
 }
+
+extension Person {
+    // 根据数据模型向数据表中插入一条模型数据
+    func insertPerson() -> Bool {
+        let sql = "INSERT INTO T_Person\n" +
+                    "(name, age, height) VALUES\n" +
+                    "('\(name!)', \(age), \(height));"
+        
+        print(sql)
+        
+        id = DZSQLiteManager.sharedManager.insertExec(sql)
+        
+        return id > 0
+    }
+    // 修改当前对象 id 对应的数据
+    func updatePerson() -> Bool {
+        let sql = "UPDATE T_Person SET name = '\(name!)', age = \(age), height = \(height) \n" +
+        "WHERE id = \(id);"
+        
+        print(sql)
+        
+        let rows = DZSQLiteManager.sharedManager.updateOrDelExec(sql)
+        
+        return rows > 0
+    }
+    // 删除当前对象 id 对应的数据
+    func deletePerson() -> Bool {
+        let sql = "DELETE FROM T_Person WHERE id = \(id)"
+        
+        print(sql)
+        
+        let rows = DZSQLiteManager.sharedManager.updateOrDelExec(sql)
+        
+        return rows > 0
+    }
+}
--- DZHandleDB/DZHandleDB/ViewController.swift
+++ DZHandleDB/DZHandleDB/ViewController.swift
@@ -13,16 +13,54 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // MARK: -测试代码
-        createTable()
+//        testCreateTable()
+//        testInsertData()
+//        testUpdateData()
+        testDeleteData()
     }
 }
 
 // MARK: - 测试函数
 extension ViewController {
     // 测试使用文件批量建表
-    func createTable() {
+    func testCreateTable() {
         
         DZSQLiteManager.sharedManager.createTableBySQLFile("demo.sql")
+    }
+    
+    // 测试插入数据操作
+    func testInsertData() {
+        let person = Person(dict: ["name": "zhang", "age": 17, "height": 1.8])
+        
+        if person.insertPerson() {
+            print("插入数据成功")
+        } else {
+            print("插入数据失败")
+        }
+        
+        print(person.id)
+    }
+    
+    // 测试更新数据
+    func testUpdateData() {
+        let person = Person(dict: ["id": 3, "name": "王五", "age": 13, "height": 1.6])
+        
+        if person.updatePerson() {
+            print("更新数据成功")
+        } else {
+            print("更新数据失败")
+        }
+    }
+    
+    // 测试删除数据
+    func testDeleteData() {
+        let person = Person(dict: ["id": 3, "name": "王五", "age": 13, "height": 1.6])
+        
+        if person.deletePerson() {
+            print("删除数据成功")
+        } else {
+            print("删除数据失败")
+        }
     }
 }
 
